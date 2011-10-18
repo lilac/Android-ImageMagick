d28222850a95cac27477bafbc6c1b543ff6fd9cc
James Deng <cnJamesDeng@gmail.com>
2011-10-18 13:38:36 +0800

    Add basic demo.

--- gen/android/magick/R.java
+++ gen/android/magick/R.java
@@ -8,10 +8,17 @@
 package android.magick;
 
 public final class R {
+    public static final class array {
+        public static final int effects=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+    }
+    public static final class id {
+        public static final int imageView=0x7f070001;
+        public static final int spinner=0x7f070000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -21,7 +28,8 @@ public final class R {
         public static final int live_folder_notes=0x7f040001;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int hello=0x7f050000;
+        public static final int app_name=0x7f060001;
+        public static final int effects_prompt=0x7f060002;
+        public static final int hello=0x7f060000;
     }
 }
--- src/magick/AndroidMagickActivity.java
+++ src/magick/AndroidMagickActivity.java
@@ -5,13 +5,20 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.magick.R;
 import magick.util.DisplayImageMetaData;
 import magick.util.MagickBitmap;
+
+import android.view.*;
 
 public class AndroidMagickActivity extends Activity {
     /** Called when the activity is first created. */
@@ -20,12 +27,50 @@ public class AndroidMagickActivity extends Activity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.main);
         //InputStream stream = getResources().openRawResource(R.raw.app_notes);
-        MagickImage img = null;
         try {
-        	img = new MagickImage(new ImageInfo("/sdcard/images.jpeg"));
+        	final MagickImage img = new MagickImage(new ImageInfo("/sdcard/images.jpeg"));
+        	/*
         	ImageView iv = new ImageView(this);
         	iv.setImageDrawable(new BitmapDrawable(getResources(), MagickBitmap.ToBitmap(img)));
         	setContentView(iv);
+        	*/
+        	setContentView(R.layout.main);
+
+            Spinner s = (Spinner) findViewById(R.id.spinner);
+            ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                    this, R.array.effects, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            s.setAdapter(adapter);
+            s.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+                public void onItemSelected(AdapterView<?> parent,
+                		View view, int pos, long id) {
+                	/*
+                  Toast.makeText(parent.getContext(), "The planet is " +
+                      parent.getItemAtPosition(pos).toString(), Toast.LENGTH_LONG).show();
+                	 */
+                	String item = parent.getSelectedItem().toString();
+                	int effect = 0;
+                	if (item.equals("Uniform Noise")) effect = NoiseType.UniformNoise;
+                	else if (item.equals("Gaussian Noise")) effect = NoiseType.GaussianNoise;
+                	Bitmap bitmap;
+					try {
+						bitmap = MagickBitmap.ToBitmap(img.addNoiseImage(effect));
+						ImageView iv = (ImageView) findViewById(R.id.imageView);
+                	iv.setImageBitmap(bitmap);
+					} catch (MagickException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+                	
+                }
+
+                public void onNothingSelected(AdapterView<?> parent) {
+                  // Do nothing.
+                }
+            });
+            ImageView iv = (ImageView) findViewById(R.id.imageView);
+            iv.setImageBitmap(MagickBitmap.ToBitmap(img));
         } catch (MagickApiException e) {
         	e.printStackTrace();
         } catch (MagickException e) {
